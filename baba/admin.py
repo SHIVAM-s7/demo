f421c2028daa1396da08465a64c9238f2e7a7fc9
Shivam Sawant <shivamsawant410@gmail.com>
2021-03-28 20:29:29 +0530

    Added To Admin

--- baba/admin.py
+++ baba/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import UserProfile, Task, TaskTag
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Task)
+admin.site.register(TaskTag)
